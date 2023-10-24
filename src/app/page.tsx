c668bbe89f5ff564c56f11215ddfa493980ce3f1
SindhujaDeveloper <crazydeveloper.p@gmail.com>
2023-10-24 13:41:02 +0530

    routing changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import { Router } from "@remix-run/router";
 import styles from "./page.module.css";
 import FaceDetection from "./pages/faceDetection";
@@ -10,24 +10,33 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 let router: Router;
-if (typeof window !== 'undefined') {
+if (typeof window !== undefined) {
   router = createBrowserRouter([
     {
       path: "/",
       element: <MainPage />,
-      children: [
-        { path:"/face-detection", element: <FaceDetection /> },
-        { path: "/single-image-comparision", element: <SingleImageComparision /> },
-        { path: "/mutiple-image-comparision", element: <MultipleImageUpload /> },
-        { path: "/face-expression-detection", element: <FaceExpressionDetection /> }
-      ],
+    },
+    {
+      path: "/face-detection",
+      element: <FaceDetection />,
+    },
+    {
+      path: "/single-image-comparision",
+      element: <SingleImageComparision />,
+    },
+    {
+      path: "/mutiple-image-comparision",
+      element: <MultipleImageUpload />,
+    },
+    {
+      path: "/face-expression-detection",
+      element: <FaceExpressionDetection />,
     },
   ]);
 }
 
 const Page = () => {
   return router ? <RouterProvider router={router} /> : null;
-}
-
+};
 
 export default Page;
