f8cdab8fde7eeee4bcf368eef8903fba33396a4e
SindhujaDeveloper <crazydeveloper.p@gmail.com>
2023-10-31 15:14:29 +0530

    face detection using live camera completed

--- src/app/pages/mainPage.tsx
+++ src/app/pages/mainPage.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useRouter } from "next/navigation";
 import React from "react";
-import { Button } from "react-bootstrap";
+import { Button, Col, Row } from "react-bootstrap";
 
 const MainPage = () => {
   const router = useRouter();
@@ -11,32 +11,54 @@ const MainPage = () => {
       <h3 style={{ marginTop: "20px" }}>
         Facial Detection, Comparison, and Expression Analysis
       </h3>
-      <div style={{ marginTop: "200px" }}>
-        <Button
-          style={{ marginLeft: "30px" }}
-          onClick={() => router.push("./face-detection")}
-        >
-          Face Detection
-        </Button>
-        <Button
-          style={{ marginLeft: "30px" }}
-          onClick={() => router.push("./face-expression-detection")}
-        >
-          Face Expression Detection
-        </Button>
-        <Button
-          style={{ marginLeft: "30px" }}
-          onClick={() => router.push("./single-image-comparision")}
-        >
-          Compare with Single Image
-        </Button>
-        <Button
-          style={{ marginLeft: "30px" }}
-          onClick={() => router.push("./mutiple-images-comparision")}
-        >
-          Compare Single face with Multiple Images with Expression
-        </Button>
-      </div>
+      <Row style={{ marginTop: "100px" }}>
+        <Col xs={6}>
+          <Button
+            style={{
+              marginLeft: "10px",
+              marginTop: "50px",
+              width: "60%",
+            }}
+            onClick={() => router.push("./face-detection")}
+          >
+            Face Detection
+          </Button>
+        </Col>
+        <Col xs={6}>
+          <Button
+            style={{ marginLeft: "0px", marginTop: "50px", width: "60%" }}
+            onClick={() => router.push("./face-expression-detection")}
+          >
+            Face Expression Detection
+          </Button>
+        </Col>
+        <Col xs={6}>
+          <Button
+            style={{ marginLeft: "0px", marginTop: "50px", width: "60%" }}
+            onClick={() => router.push("./single-image-comparision")}
+          >
+            Compare with Single Image
+          </Button>
+        </Col>
+        <Col xs={6}>
+          <Button
+            style={{ marginLeft: "10px", marginTop: "50px", width: "60%" }}
+            onClick={() => router.push("./mutiple-images-comparision")}
+          >
+            Compare Single face with Multiple Images with Expression
+          </Button>
+        </Col>
+        <Col xs={12}>
+          <Button
+            style={{ marginLeft: "10px", marginTop: "50px", width: "40%" }}
+            name="Ravi only coordinates with Radha, which is sufficient for Radha. She
+          will handle everything."
+            onClick={() => router.push("./face-detection-with-live-camera")}
+          >
+            Face Detection using Live Camera
+          </Button>
+        </Col>
+      </Row>
     </div>
   );
 };
